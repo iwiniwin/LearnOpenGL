dcf9c8c8d7ef79fc71bd044e84a2cde0f7745d4b
iwiniwin <iwiniwin@foxmail.com>
2020-08-10 21:31:30 +0800

    创建多重采样缓冲实现离屏MSAA

--- LearnOpenGL/include/scenes/anti_aliasing_scene.h
+++ LearnOpenGL/include/scenes/anti_aliasing_scene.h
@@ -14,6 +14,7 @@ public:
 	float height;
 	Shader* cubeShader;
 	unsigned int cubeVAO;
+	unsigned int FBO;
 
 	virtual Camera* getCamera() { return &camera; }
 
@@ -22,9 +23,13 @@ public:
 		this->height = height;
 		camera = Camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-		glEnable(GL_MULTISAMPLE);  // 启用多重采样，想要多重采样生效还需要在窗口创建前调用glfwWindowHint(GLFW_SAMPLES, 4);
+		glEnable(GL_MULTISAMPLE);  // 启用多重采样，
+								   // 想要多重采样生效还需要在窗口创建前调用glfwWindowHint(GLFW_SAMPLES, 4);
+								   // 或者创建自己的帧缓冲
 		cubeShader = new Shader("shaders\\shader.vs", "shaders\\single_color_shader.fs");
 		initVAO();
+
+		initFBO();
 	}
 
 	void initVAO() {
@@ -44,7 +49,37 @@ public:
 
 	}
 
+	void initFBO() {
+		glGenFramebuffers(1, &FBO);
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+		// 创建多重采样颜色附件
+		unsigned int textureColorBufferMultisample;
+		glGenTextures(1, &textureColorBufferMultisample);
+		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultisample);
+		// 创建一个支持多个采样点的纹理，4表示采样点个数
+		glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGB, width, height, GL_TRUE);
+		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
+		// 将多重采样纹理加到帧缓冲上
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultisample, 0);
+		
+		// 为深度和模板创建多重采样rbo
+		unsigned int rbo;
+		glGenRenderbuffers(1, &rbo);
+		glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+		// 创建多重采样渲染缓冲对象
+		glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8, width, height);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+			cerr << "check framebuffer error" << endl;
+		}
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
 	virtual void update(GLFWwindow* window, float deltaTime) {
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		// 清除颜色缓冲和深度缓冲
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -58,8 +93,14 @@ public:
 		cubeShader->setMat4("view", view);
 		cubeShader->setMat4("projection", projection);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, FBO);
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+		glBlitFramebuffer(0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
 	}
 	virtual void destroy() {
-
+		delete(cubeShader);
 	}
 };
