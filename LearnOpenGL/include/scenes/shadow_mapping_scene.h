a6fc7709d2d8816d09612c28bec443f395d67e33
iwiniwin <iwiniwin@foxmail.com>
2020-08-14 13:59:31 +0800

    添加生成深度贴图部分代码

--- LearnOpenGL/include/scenes/shadow_mapping_scene.h
+++ LearnOpenGL/include/scenes/shadow_mapping_scene.h
@@ -18,6 +18,9 @@ public:
 	Shader* cubeShader;
 	unsigned int cubeVAO;
 
+	unsigned int depthMapFBO;
+	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;  // 深度贴图的分辨率
+
 	virtual Camera* getCamera() { return &camera; }
 
 	virtual void init(GLFWwindow* window, float width, float height) {
@@ -28,6 +31,8 @@ public:
 		shader = new Shader("shaders\\shader.vs", "shaders\\shadow_mapping.fs");
 		cubeShader = new Shader("shaders\\shader.vs", "shaders\\single_tex_shader.fs");
 		initVAO();
+
+		//initFBO();
 
 		glEnable(GL_DEPTH_TEST);
 	}
@@ -66,6 +71,28 @@ public:
 		glBindVertexArray(0);
 	}
 
+	void initFBO() {
+		
+		glGenFramebuffers(1, &depthMapFBO);
+
+		// 创建一个2D纹理提供给帧缓冲的深度缓冲使用
+		unsigned int depthMap;
+		glGenTextures(1, &depthMap);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+		// 仅用于渲染深度贴图，所以颜色缓冲没有用，但是帧缓冲必须要有个颜色缓冲
+		// 通过将读和绘制缓冲设置为NONE，来告诉OpenGL我们不使用任何颜色数据来进行渲染
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
 	virtual void update(GLFWwindow* window, float deltaTime) {
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		// 清除颜色缓冲和深度缓冲
@@ -74,13 +101,24 @@ public:
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), width / height, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
 		glm::mat4 model = glm::mat4(1.0f);
-		shader->setMat4("model", model);
-
 		glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+
+		// 1. 渲染深度贴图
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);  // 记得调用，因为阴影贴图经常和窗口的分辨率不一样
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+
+
+		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		// 2. 像往常一样渲染场景，同时使用上一步生成的深度贴图
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// 绘制地板
 		shader->use();
 		shader->setMat4("projection", projection);
 		shader->setMat4("view", view);
-
+		shader->setMat4("model", model);
 		glBindVertexArray(VAO);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE0, texture);
