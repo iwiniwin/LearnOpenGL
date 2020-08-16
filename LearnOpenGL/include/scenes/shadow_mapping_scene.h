0deefa5bce406f97f3cf26e0edcf4e712007039b
iwiniwin <iwiniwin@foxmail.com>
2020-08-16 23:56:42 +0800

    添加绘制深度贴图部分代码

--- LearnOpenGL/include/scenes/shadow_mapping_scene.h
+++ LearnOpenGL/include/scenes/shadow_mapping_scene.h
@@ -5,6 +5,8 @@
 
 extern float planeVertices2[];
 extern int planeVertices2Size;
+extern float fullScreenQuadVertices[];
+extern int fullScreenQuadVerticesSize;
 
 class ShadowMappingScene : public Scene {
 
@@ -17,8 +19,13 @@ public:
 	unsigned int texture;
 	Shader* cubeShader;
 	unsigned int cubeVAO;
+	Shader* depthShader;
+	Shader* quadShder;
+
+	unsigned int quadVAO;
 
 	unsigned int depthMapFBO;
+	unsigned int depthMap;
 	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;  // 深度贴图的分辨率
 
 	virtual Camera* getCamera() { return &camera; }
@@ -28,11 +35,13 @@ public:
 		this->height = height;
 		camera = Camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
+		depthShader = new Shader("shaders\\simple_depth_shader.vs", "shaders\\empth.fs");
 		shader = new Shader("shaders\\shader.vs", "shaders\\shadow_mapping.fs");
 		cubeShader = new Shader("shaders\\shader.vs", "shaders\\single_tex_shader.fs");
+		quadShder = new Shader("shaders\\ndc_shader.vs", "shaders\\single_tex_shader.fs");
 		initVAO();
 
-		//initFBO();
+		initFBO();
 
 		glEnable(GL_DEPTH_TEST);
 	}
@@ -69,6 +78,21 @@ public:
 		glEnableVertexAttribArray(2);
 		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 		glBindVertexArray(0);
+
+		glGenVertexArrays(1, &quadVAO);
+		unsigned int screenVBO;
+		glGenBuffers(1, &screenVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, screenVBO);
+		glBufferData(GL_ARRAY_BUFFER, fullScreenQuadVerticesSize, &fullScreenQuadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+		glBindVertexArray(0);
+
+		this->quadShder->use();
+		this->quadShder->setInt("texture1", 0);
 	}
 
 	void initFBO() {
@@ -76,7 +100,6 @@ public:
 		glGenFramebuffers(1, &depthMapFBO);
 
 		// 创建一个2D纹理提供给帧缓冲的深度缓冲使用
-		unsigned int depthMap;
 		glGenTextures(1, &depthMap);
 		glBindTexture(GL_TEXTURE_2D, depthMap);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
@@ -98,42 +121,90 @@ public:
 		// 清除颜色缓冲和深度缓冲
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		
+		glm::vec3 lightPos(-2.0f, 4.0f, -1.0f);
+
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), width / height, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
 		glm::mat4 model = glm::mat4(1.0f);
-		glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+
+		// 因为是一个所有光线都平行的定向光，所以使用正交投影矩阵
+		glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 1.0f, 7.5f);
+		glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+
+		depthShader->use();
+		depthShader->setMat4("lightSpaceMatrix", lightSpaceMatrix);
 
 		// 1. 渲染深度贴图
 		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);  // 记得调用，因为阴影贴图经常和窗口的分辨率不一样
 		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-
-
-		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-		// 2. 像往常一样渲染场景，同时使用上一步生成的深度贴图
-		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// 绘制地板
-		shader->use();
-		shader->setMat4("projection", projection);
-		shader->setMat4("view", view);
-		shader->setMat4("model", model);
-		glBindVertexArray(VAO);
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE0, texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		depthShader->setMat4("model", model);
+
+		// 绘制地板
+		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		// 绘制立方体
-		cubeShader->use();
-		cubeShader->setMat4("model", model);
-		cubeShader->setMat4("view", view);
-		cubeShader->setMat4("projection", projection);
-		cubeShader->setVec3("cameraPos", camera.Position);
 		glBindVertexArray(cubeVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-	}
-	virtual void destroy() {
 
+		// 调试
+		// 绘制深度贴图到四边形
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glBindVertexArray(quadVAO);
+		quadShder->use();
+		//quadShder
+		glActiveTexture(0);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+		glBindVertexArray(0);
+
+
+		// 2. 像往常一样渲染场景，同时使用上一步生成的深度贴图
+
+		
+
+		// 绘制地板
+		//shader->use();
+		//shader->setMat4("projection", projection);
+		//shader->setMat4("view", view);
+		//shader->setMat4("model", model);
+		//glBindVertexArray(VAO);
+		//glActiveTexture(GL_TEXTURE0);
+		//glBindTexture(GL_TEXTURE_2D, texture);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		//// 绘制立方体
+		//cubeShader->use();
+		//cubeShader->setMat4("model", model);
+		//cubeShader->setMat4("view", view);
+		//cubeShader->setMat4("projection", projection);
+		//cubeShader->setVec3("cameraPos", camera.Position);
+		//glBindVertexArray(cubeVAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
+	void renderScene() {
+		
+		
+	}
+
+
+	virtual void destroy() {
+		delete(shader);
+		delete(depthShader);
+		delete(cubeShader);
+		delete(quadShder);
 	}
 };
